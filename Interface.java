5da98ac6c68e5f30e6e5f24c88cf0d9884bd3ae8
cadu321r <cadurocha39@gmail.com>
2024-05-11 11:42:41 -0300

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-```java
 interface Interligar{
     void informacao();
 }
-```
-=======
-interface Interligar{
-    void informacao();
-}
->>>>>>> 2181dbb68489f82342a41a2e757b8ec49f33a5fc
