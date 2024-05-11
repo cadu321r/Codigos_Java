49f56c1a3edc05ec009fe012cded0d7ace951c24
cadu321r <cadurocha39@gmail.com>
2024-05-11 11:44:18 -0300

    Update Contato_Pessoal.java

--- Contato_Pessoal.java
+++ Contato_Pessoal.java
@@ -23,30 +23,3 @@ class ContatoPessoal extends Contato{
         System.out.println("Contatos Pessoais");
     }
 }
-```
-=======
-class ContatoPessoal extends Contato{
-    private String endereco;
-    private String dataAniversario;
-    
-    // construtor
-    ContatoPessoal(String name, String email, String telefone, String endereco, String dataAniversario){
-        super(name, email, telefone);
-        this.endereco = endereco;
-        this.dataAniversario = dataAniversario;
-    }
-    
-    String getEndereco(){
-        return endereco;
-    }
-    
-    String getDataAniversario(){
-        return dataAniversario;
-    }
-    
-    // Método implementado da interface
-    @Override
-    public void informacao(){
-        System.out.println("Contatos Pessoais");
-    }
-}
