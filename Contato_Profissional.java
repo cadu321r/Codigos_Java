1975805d9c3cca9c1d6919bf4c301951abedff01
cadu321r <cadurocha39@gmail.com>
2024-05-11 11:35:10 -0300

    Update Contato_Profissional.java

--- Contato_Profissional.java
+++ Contato_Profissional.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ```java
 class ContatoProfissional extends Contato{
     private String empresa;
@@ -25,3 +26,30 @@ class ContatoProfissional extends Contato{
     }
 }
 ```
+=======
+class ContatoProfissional extends Contato{
+    private String empresa;
+    private String cargo;
+    
+    // Construtor
+    ContatoProfissional(String name, String email, String telefone, String empresa, String cargo){
+        super(name, email, telefone);
+        this.empresa = empresa;
+        this.cargo = cargo;
+    }
+    
+    String getEmpresa(){
+        return empresa;
+    }
+    
+    String getCargo(){
+        return cargo;
+    }
+    
+    // Método implementado da interface
+    @Override
+    public void informacao(){
+        System.out.println("Contatos Profissionais");
+    }
+}
+>>>>>>> 2181dbb68489f82342a41a2e757b8ec49f33a5fc
