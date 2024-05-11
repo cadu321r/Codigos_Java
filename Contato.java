07d4e3fbd72666c86be70abb2143235f2962b80e
cadu321r <cadurocha39@gmail.com>
2024-05-11 11:44:21 -0300

    Update Contato.java

--- Contato.java
+++ Contato.java
@@ -29,36 +29,3 @@ class Contato implements Interligar {
         System.out.println("Contatos Gerais");
     }
 }
-```
-=======
-class Contato implements Interligar {
-    private String name;
-    private String email;
-    private String telefone;
-
-    // Construtor
-    Contato(String name, String email, String telefone){
-        this.name = name;
-        this.email = email;
-        this.telefone = telefone;
-    }
-
-    // Métodos de acesso
-    String getName() {
-        return name;
-    }
-
-    String getEmail() {
-        return email;
-    }
-
-    String getTelefone() {
-        return telefone;
-    }
-
-    // Método implementado da interface
-    @Override
-    public void informacao() {
-        System.out.println("Contatos Gerais");
-    }
-}
